8d8ec81cb505643b7f9391f01bf657f309e986ff
JEON-HYERIN <jeonhyelin19951118@gmail.com>
2021-09-07 20:28:19 +0900

    CSS: WIT

---
+++ js/main.js
@@ -0,0 +1,22 @@
+new Swiper('.swiper.visual-swiper', {
+  loop: true,
+  autoplay: true,
+  navigation: {
+    prevEl: '.swiper-prev',
+    nextEl: '.swiper-next'
+  }
+});
+
+new Swiper('.swiper.sub-swiper', {
+  loop: true,
+  // autoplay: {
+  //   delay: 5000
+  // },
+  slidesPerView: 3.5,
+  // spaceBetween: 5,
+  centeredSlides: true,
+  navigation: {
+    prevEl: '.swiper-prev',
+    nextEl: '.swiper-next'
+  }
+});
